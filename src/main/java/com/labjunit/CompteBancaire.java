53caeac8f5c7ba51e5e2be23978070feeb49fb92
Your Name <you@example.com>
2024-05-02 09:20:02 +0200

    test jenkins

--- src/main/java/com/labjunit/CompteBancaire.java
+++ src/main/java/com/labjunit/CompteBancaire.java
@@ -2,10 +2,12 @@ package main.java.com.labjunit;
 
 public class CompteBancaire {
 
+    // Créditer solde
     public double crediterSolde(double montant, double solde) {
         return solde + montant;
     }
 
+    // Débiter solde
     public double debiterSolde(double montant, double solde) {
         return solde - montant;
     }
